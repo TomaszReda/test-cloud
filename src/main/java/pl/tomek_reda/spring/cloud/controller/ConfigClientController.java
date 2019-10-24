7bf23c7822e0be43f11f1ea8bf75309e87ec29bc
tomek <GHJbnm123>
2019-10-24 19:27:44 +0200

    TEST

---
+++ src/main/java/pl/tomek_reda/spring/cloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package pl.tomek_reda.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${info.property}")
+    private String property;
+
+    @GetMapping("/message")
+    String property() {
+        return property;
+    }
+
+}
