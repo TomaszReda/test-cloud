8017898e8f93013f82df56fc85590154448ee568
tomek <GHJbnm123>
2019-10-25 10:51:26 +0200

    TEST

--- src/main/java/pl/tomek_reda/spring/cloud/controller/ConfigClientController.java
+++ src/main/java/pl/tomek_reda/spring/cloud/controller/ConfigClientController.java
@@ -3,15 +3,18 @@ package pl.tomek_reda.spring.cloud.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
 public class ConfigClientController {
-    @Value("${info.property}")
-    private String property;
-    @GetMapping("/message")
-    String property() {
-        return property;
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
     }
 }
